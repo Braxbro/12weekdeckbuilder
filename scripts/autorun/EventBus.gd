9bba37baddeac46dfb9d6e0e37ae29651ee02b96
Braxbro <63511112+Braxbro@users.noreply.github.com>
2025-08-28 18:48:54 -0400

    add notes to autoruns

--- scripts/autorun/EventBus.gd
+++ scripts/autorun/EventBus.gd
@@ -1,1 +1,6 @@
 extends Object
+
+# This file should ONLY contain connections between major features to make routing events cleaner and more flexible.
+# Direct parent-to-child relations, like a deck communicating with a card, should not go through this.
+
+# For example, the deck handler asking the timeline to schedule something would be a good use for this file.
--- scripts/autorun/globals.gd
+++ scripts/autorun/globals.gd
@@ -1,1 +1,4 @@
 extends Object
+
+# This file exposes common values and methods as global values.
+# There should be no variables in here.
