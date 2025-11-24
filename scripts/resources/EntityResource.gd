7d139da2046c54ea76ba8165bd79f6d7b7e32dc0
Braxbro <63511112+Braxbro@users.noreply.github.com>
2025-11-24 01:42:59 -0500

    First commit of Modifier classes, misc. early changes for deck management

---
+++ scripts/resources/EntityResource.gd
@@ -0,0 +1,14 @@
+class_name EntityResource extends Resource
+
+#Currently a placeholder class
+
+## Stores the entity's side for use in effects it casts.
+var side: Globals.CasterSide
+
+## Stores the entity's index in its side's array.
+var list_index: int
+
+
+
+
+#TODO: Implement class according to needs of EffectResources.
